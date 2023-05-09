afa8b5d8693a4f1279ab70c61a40525e35f2d858
ArunaAP <it20657796@my.sliit.lk>
2023-05-09 13:04:58 +0530

    Event handle in class and functional com

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import Greet from './Components/Greet';
 import Counter from './Components/Counter';
 import Message from './Components/Message';
+import FunctionClick from './Components/FunctionClick';
+import ClassClick from './Components/ClassClick';
 
 function App() {
   return (
     <div className="App">
+      <FunctionClick />
+      <ClassClick />
+
       {/* <Message /> */}
 
       {/* <Counter /> */}
 
-      <Greet name="Aruna" heroName ="Super man"/>
+      {/* <Greet name="Aruna" heroName ="Super man"/>
       <Greet name="Priyankara" heroName ="Spider Man"/>
-      <Greet name="John" heroName ="Iron Man"/>
+      <Greet name="John" heroName ="Iron Man"/> */}
     </div>
   );
 }
