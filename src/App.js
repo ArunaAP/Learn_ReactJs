94324cd4e743982a565b7a5675bed011e66e3e40
ArunaAP <it20657796@my.sliit.lk>
2023-05-09 11:43:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+// import Greet from './Components/Greet';
+import Counter from './Components/Counter'
+
+function App() {
+  return (
+    <Counter/>
+
+    // <div className="App">
+    //   <Greet name="Aruna"/>
+    //   <Greet name="Priyankara"/>
+    //   <Greet name="John"/>
+    // </div>
+  );
+}
+
+export default App;
